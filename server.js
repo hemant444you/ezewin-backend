46fa79a6437df6febc652e04ab9d8727ca3d95bf
Hemant Kumar <krguptahemant@gmail.com>
2024-02-07 01:10:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/',function(req, res){
+    res.send('its working'); 
+});
+
+app.listen(process.env.PORT || 8000);
+module.export = app;
