6383e87b7ea35d47be08713fc5c3acc2ef0a0ab2
Hemant Kumar <krguptahemant@gmail.com>
2024-02-07 19:27:44 +0530

    test

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get('/',function(req,res){
     res.send('its working');
-})
+});
+
+app.get('/test',function(req,res){
+    res.send('its working');
+});
+
 app.listen(process.env.PORT || 8000);
 module.export = app
